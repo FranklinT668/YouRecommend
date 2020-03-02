a0c6953b5cffd171dd5cbf48a5150b1065467dec
Yaruo Tian <tiany38@mcmaster.ca>
2020-03-02 11:51:40 -0500

    added test.csv for testing small set, search function work in progress

--- YouRecommend/src/Search.java
+++ YouRecommend/src/Search.java
@@ -1,11 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 
 public class Search {
 
 	public static void main(String[] args) {
-		// Franklin's Section
-		
-		System.out.println("Franklin searching");
 
-	}
+        String fileName = "test.csv";
+        File file = new File(fileName);
+        
+		try {
+			Scanner inputStream = new Scanner(file);
+			while(inputStream.hasNextLine()) {
+				String data = inputStream.nextLine();
+				String[] values = data.split(",");
+				System.out.println(values[6] + "***");
+			}
+			inputStream.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+       
+
+    }
 
 }
