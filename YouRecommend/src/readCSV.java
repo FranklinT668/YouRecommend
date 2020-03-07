23e39a2321f6cf578583260f16961c3e5ac6ee9c
Yaruo Tian <tiany38@mcmaster.ca>
2020-03-07 14:26:34 -0500

    readCSV working

--- YouRecommend/src/readCSV.java
+++ YouRecommend/src/readCSV.java
@@ -1,28 +1,66 @@
 import java.io.File;
+
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class readCSV {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedReader;
 
+
+public class readCSV {
 	public static void main(String[] args) {
-		System.out.println("TEST");
-        String fileName = "test.csv";
-        File file = new File(fileName);
-        
-		try {
-			Scanner inputStream = new Scanner(file);
-			while(inputStream.hasNextLine()) {
-				String data = inputStream.nextLine();
-				String[] values = data.split(",");
-				System.out.println(values[0] + ", " + values[1] + ", " + values[2] + ", " + values[3] + ", " + values[4] + ", " + values[5]);
-			}//not sure why it error index 5 out of bounds for length 5 
-			inputStream.close();
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		// Receive input of text file
+		String input = "test.csv";
+		String[][] dataset = CSVReader(input);
+
+		int n = dataset.length-1;
+
+		for(int i=0;i<n;i++) { 
+			System.out.print("\n" + i + " ");
+			for(int j=0; j<6; j++) {
+				System.out.print(dataset[i][j] + " "); 
+				}
+			}		
+		
 		}
 
-	}
+	public static String[][] CSVReader(String filepath) {
 
+	     String csvFile = filepath;
+	     BufferedReader br = null;
+	     String line = "";
+	     String[][] dataset = new String[104540][6];
+	     int row = 0; 		
+	     
+	     try {
+
+	         br = new BufferedReader(new FileReader(csvFile));
+	         while ((line = br.readLine()) != null) {
+	         	
+	         	String temp = line;
+	         	dataset[row] = temp.split(",");
+	         	row++;
+	         }
+
+	     } catch (FileNotFoundException e) {
+	         e.printStackTrace();
+	     } catch (IOException e) {
+	         e.printStackTrace();
+	     } finally {
+	         if (br != null) {
+	             try {
+	                 br.close();
+	             } catch (IOException e) {
+	                 e.printStackTrace();
+	             }
+	         }
+	     }
+	     
+	     return dataset;
+	 }
 }
