fa2bb6af373e1ddb2228f076954358bcc0596375
Yaruo Tian <tiany38@mcmaster.ca>
2020-03-10 10:15:58 -0400

    search functional, use smallSetTest.csv to test with a smaller dataset

--- YouRecommend/src/readCSV.java
+++ YouRecommend/src/readCSV.java
@@ -11,56 +11,55 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedReader;
 
-
 public class readCSV {
 	public static void main(String[] args) {
 		// Receive input of text file
 		String input = "test.csv";
 		String[][] dataset = CSVReader(input);
 
-		int n = dataset.length-1;
+		int n = dataset.length - 1;
 
-		for(int i=0;i<n;i++) { 
+		for (int i = 1; i < n; i++) { // i starts at 1 to skip first line in dataset
 			System.out.print("\n" + i + " ");
-			for(int j=0; j<6; j++) {
-				System.out.print(dataset[i][j] + " "); 
-				}
-			}		
-		
+			for (int j = 0; j < 6; j++) {
+				System.out.print(dataset[i][j] + ", ");
+			}
 		}
+
+	}
 
 	public static String[][] CSVReader(String filepath) {
 
-	     String csvFile = filepath;
-	     BufferedReader br = null;
-	     String line = "";
-	     String[][] dataset = new String[104540][6];
-	     int row = 0; 		
-	     
-	     try {
+		String csvFile = filepath;
+		BufferedReader br = null;
+		String line = "";
+		String[][] dataset = new String[104540][6];
+		int row = 0;
 
-	         br = new BufferedReader(new FileReader(csvFile));
-	         while ((line = br.readLine()) != null) {
-	         	
-	         	String temp = line;
-	         	dataset[row] = temp.split(",");
-	         	row++;
-	         }
+		try {
 
-	     } catch (FileNotFoundException e) {
-	         e.printStackTrace();
-	     } catch (IOException e) {
-	         e.printStackTrace();
-	     } finally {
-	         if (br != null) {
-	             try {
-	                 br.close();
-	             } catch (IOException e) {
-	                 e.printStackTrace();
-	             }
-	         }
-	     }
-	     
-	     return dataset;
-	 }
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+
+				String temp = line;
+				dataset[row] = temp.split(",");
+				row++;
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return dataset;
+	}
 }
