603e0cb912186d5f83509f1638c0eae4572d6545
Yaruo Tian <tiany38@mcmaster.ca>
2020-04-07 15:41:13 -0400

    documented search and readCSV module, removed unnecessary test functions

--- YouRecommend/src/Search.java
+++ YouRecommend/src/Search.java
@@ -11,43 +11,54 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedReader;
 
+/**
+ * @brief An ADT that performs search in the database.
+ */
 public class Search {
 
+	/**
+	 * @brief Function that remove all white spaces from a string
+	 * @param input The string which whitespace are to be removed
+	 * @return The string without whitespace
+	 */
 	public static String removeWhiteSpaces(String input) {
 		return input.replaceAll("\\s+", "");
 	}
 
+	/**
+	 * @brief Driver function that displays the all the channel information being
+	 *        searched
+	 */
 	public static void main(String[] args) {
-		// Receive input of text file
+		// Receive input of the dataset csv file
 		String input = "test.csv";
+		// Stores the input in a 2D array
 		String[][] dataset = readCSV.CSVReader(input);
-
 		int n = dataset.length - 1;
-		String searchInput = new String("ironcladindustries");//input
+		// Search Youtube channel as input
+		String searchInput = new String("Pew Die Pie");
 		boolean founded = false;
 		int foundIndex = 0;
-
-		for (int i = 1; i < n; i++) { // i starts at 1 to skip first line in dataset
-//			System.out.print( i + " ");
-//			
-//			System.out.println(dataset[i][4]); 
-
-			if (searchInput.equalsIgnoreCase(dataset[i][4])//case not sensitive and ignores white spaces
+		// Sequential search in the dataset to match searchInput to the corresponding
+		// youtube channels
+		for (int i = 1; i < n; i++) {
+			// The input's case is not sensitive and ignores white spaces
+			if (searchInput.equalsIgnoreCase(dataset[i][4])
 					|| removeWhiteSpaces(searchInput).equalsIgnoreCase(removeWhiteSpaces(dataset[i][4]))) {
 				founded = true;
 				foundIndex = i;
 			}
 		}
 		System.out.println("YOUTUBER EXISTS: " + founded);
-		// System.out.println(foundIndex);
-		if (founded) {// exists within database
+		// Channel exists within the database
+		if (founded) {
 			System.out.println("CATEGORY: " + dataset[foundIndex][1]);
 			System.out.println("SUBSCRIBERS: " + dataset[foundIndex][2]);
 			System.out.println("JOIN DATE: " + dataset[foundIndex][3]);
 			System.out.println("CHANNEL NAME: " + dataset[foundIndex][4]);
 			System.out.println("VIDEO COUNT: " + dataset[foundIndex][5]);
-
-		} else {// doesnt exists within database
+			// Channel does not exist within the database
+		} else {
 			System.out.print("YOUTUBER INFORMATION: N/A");
 		}
 	}
--- YouRecommend/src/readCSV.java
+++ YouRecommend/src/readCSV.java
@@ -11,23 +11,16 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedReader;
 
+/**
+ * @brief readCSV module to read from the database
+ */
 public class readCSV {
-	public static void main(String[] args) {
-		// Receive input of text file
-		String input = "test.csv";
-		String[][] dataset = CSVReader(input);
 
-		int n = dataset.length - 1;
-
-		for (int i = 1; i < n; i++) { // i starts at 1 to skip first line in dataset
-			System.out.print("\n" + i + " ");
-			for (int j = 0; j < 6; j++) {
-				System.out.print(dataset[i][j] + ", ");
-			}
-		}
-
-	}
-
+	/**
+	 * @brief Manipulate the data from csv file to usable format
+	 * @param filepath The file to be read from
+	 * @return The data that is stored in a 2D array
+	 */
 	public static String[][] CSVReader(String filepath) {
 
 		String csvFile = filepath;
@@ -36,21 +29,23 @@ public class readCSV {
 		String[][] dataset = new String[104540][6];
 		int row = 0;
 
+		// Uses BufferedReader to read the text from filepath
 		try {
-
 			br = new BufferedReader(new FileReader(csvFile));
+			// If there are still lines within the database
 			while ((line = br.readLine()) != null) {
-
+				// Splits each line by "," and stores it in dataset array
 				String temp = line;
 				dataset[row] = temp.split(",");
 				row++;
 			}
-
+			// Checks are errors
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
+			// Closes the file when everything is stored in dataset array
 			if (br != null) {
 				try {
 					br.close();
@@ -59,7 +54,6 @@ public class readCSV {
 				}
 			}
 		}
-
 		return dataset;
 	}
 }
