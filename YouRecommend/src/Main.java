eefb1c4bbfb372b53727029a94f2c585ef4fe5fb
Jiawei Yu <yujiawei@yujiaweideMacBook-Pro.local>
2020-04-09 16:39:54 -0400

    update main

--- YouRecommend/src/Main.java
+++ YouRecommend/src/Main.java
@@ -2,17 +2,35 @@ import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static ArrayList<YouTuber> SortedByFollowers(String category) {
 		
 		Graph g = readCSV.generateGraph();
-		//using dfs to search according to the category "film and animation"
-		DFS d = new DFS(g, "Film and Animation");
+		//using dfs to search according to the category "film and animation".
+		DFS d = new DFS(g, category);
 		//gets related YouTubers
 		ArrayList<YouTuber> a = d.relatedYouTubers();
 		
 		Sort.MergeSort_Followers(a);
 		
-		System.out.println(a.get(1).getFollowers());
+		return a;
 	}
-
+	
+	public static ArrayList<YouTuber> SortedByVideos(String category) {
+		
+		Graph g = readCSV.generateGraph();
+		//using dfs to search according to the category "film and animation".
+		DFS d = new DFS(g, category);
+		//gets related YouTubers
+		ArrayList<YouTuber> a = d.relatedYouTubers();
+		
+		Sort.QuickSort_Videos(a);
+		
+		return a;
+	}
+	
+//	public static void main(String[] args) {
+//		ArrayList<YouTuber> a = Main.SortedByVideos("Gaming");
+//		System.out.println(a.get(0).getTitle());
+//	}
+	
 }
