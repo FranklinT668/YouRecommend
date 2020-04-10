010313251601b29ead25c8822c7464a7b4d5322b
Kabishan <suvendrk@mcmaster.ca>
2020-04-10 02:23:07 -0400

    Added radio buttons to decide between sorting methods

--- YouRecommend/src/FrontEndServlet.java
+++ YouRecommend/src/FrontEndServlet.java
@@ -6,15 +6,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.InputStream;
-
 public class FrontEndServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		PrintWriter out = res.getWriter();
 		String genre = req.getParameter("pref");
-		ArrayList<YouTuber> gaming = Main.sortedByFollowers(genre);
-		for (int i = 0; i < 100; i++) {
-			out.println(gaming.get(i).getTitle() + "\t" + gaming.get(i).getFollowers());
+		ArrayList<YouTuber> list = new ArrayList<>();
+		
+		if (req.getParameter("flw-vid").equals("0")) {
+			list = Main.sortedByFollowers(genre);
+			for (int i = 0; i < 100; i++) {
+				out.println(list.get(i).getTitle() + "\t" + list.get(i).getFollowers());
+			}
+		}
+		else if (req.getParameter("flw-vid").equals("1")) {
+			list = Main.sortedByVideos(genre);
+			for (int i = 0; i < 100; i++) {
+				out.println(list.get(i).getTitle() + "\t" + list.get(i).getVideos());
+			}
 		}
 	}
 }
